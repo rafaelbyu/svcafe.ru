aaeecf93c184e9b42824be7b17c7251928ed9a15
rafaelbyu <rafael.byu@gmail.com>
2021-01-10 12:03:26 +0300

    third commit поменял почту и пароль в mail.php еще раз
    Signed-off-by: rafaelbyu <rafael.byu@gmail.com>

--- php/mail.php
+++ php/mail.php
@@ -14,12 +14,12 @@ $smth = $_POST['smth'];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'rafael.byu@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'CMJG3EQM'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'Svcafe2012@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'Kruglova10981'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('rafael.byu@mail.ru'); // от кого будет уходить письмо?
+$mail->setFrom('Svcafe2012@mail.ru'); // от кого будет уходить письмо?
 $mail->addAddress('olga.kasatkina81@mail.ru');     // Кому будет уходить письмо
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
